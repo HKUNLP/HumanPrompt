d0533ffe134077f418532a4f440529fded71a3d8
BlankCheng <913501223@qq.com>
2022-11-09 15:53:03 +0800

    Merge conflicts

--- unifiedhumanprompt/tasks/dataset_loader.py
+++ unifiedhumanprompt/tasks/dataset_loader.py
@@ -1,5 +1,5 @@
 import os
-from typing import Any, Union
+from typing import Any, Union, List, Dict
 
 import datasets
 from datasets import Dataset, DatasetDict, IterableDataset, IterableDatasetDict
@@ -10,16 +10,18 @@ DIR = os.path.join(os.path.dirname(__file__))
 class DatasetLoader(object):
     """Dataset loader class."""
 
+    # Datasets implemented in this repo
+    # Either they do not exist in the datasets library or they have something improper
     own_dataset = {
-        # Datasets implemented in this repo,
-        # either they do not exist in the datasets library or they have something improper
         "strategy_qa": os.path.join(DIR, "strategy_qa.py"),
+        "svamp": os.path.join(DIR, "svamp.py"),
     }
 
     @staticmethod
     def load_dataset(
-        dataset_name: str, **kwargs: Any
-    ) -> Union[DatasetDict, Dataset, IterableDatasetDict, IterableDataset]:
+            dataset_name: str,
+            **kwargs: Any
+    ) -> Union[datasets.Dataset, List, Dict]:
         """
         Load dataset from the datasets library or from this repo.
         Args:
@@ -29,10 +31,13 @@ class DatasetLoader(object):
         Returns: dataset
 
         """
+        # TODO: Add warning for strategyqa test set
         # Check whether the dataset is in the own_dataset dictionary
         if dataset_name in DatasetLoader.own_dataset.keys():
             dataset_path = DatasetLoader.own_dataset[dataset_name]
             return datasets.load_dataset(dataset_path, **kwargs)
         else:
             # If not, load it from the datasets library
+            if dataset_name == "gsm8k":
+                kwargs["name"] = "main"
             return datasets.load_dataset(dataset_name, **kwargs)
