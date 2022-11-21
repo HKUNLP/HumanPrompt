ec0802d5c225b264df5597523bc5c50f0d545400
Timothyxxx <384084775@qq.com>
2022-11-21 17:45:31 +0800

    Fix errors

--- tests/test_method.py
+++ tests/test_method.py
@@ -1,20 +1,31 @@
 import pytest
 
 from humanprompt.methods.auto.method_auto import AutoMethod
+from humanprompt.tasks.dataset_loader import DatasetLoader
 
 
 @pytest.mark.usefixtures("openai_api_key")
 def test_init() -> None:
-    method = AutoMethod.from_config(method_name="cot")
+    method = AutoMethod.from_config("cot")
+    # todo, add all methods
     assert method
 
 
 @pytest.mark.usefixtures("openai_api_key")
 def test_run() -> None:
+    # todo, add all methods
+
+    # CoT
+    # commonsense_qa
     method = AutoMethod.from_config("cot")
-    prediction = method.run(
-        {
-            "question": "Were Scott Derrickson and Ed Wood of the same nationality?",
-        }
-    )
-    assert prediction == "yes"
+    data_item = DatasetLoader.load_dataset(
+        dataset_name="commonsense_qa",
+        dataset_split="validation",
+        dataset_key_map={
+            "question": "question",
+            "choices": "choices",
+            "answer": "answerKey",
+            "id": "id",
+        },
+    )[0]
+    method.run(data_item)
