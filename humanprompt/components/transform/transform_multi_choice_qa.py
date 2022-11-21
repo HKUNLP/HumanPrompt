92e53761797faee28a146faa5a969b97917cb4e3
BlankCheng <913501223@qq.com>
2022-11-21 13:35:36 +0800

    Add multi-choice-qa transform

---
+++ humanprompt/components/transform/transform_multi_choice_qa.py
@@ -0,0 +1,47 @@
+from typing import Any, Dict, Union
+
+from .transform_base import Transform
+
+
+class MultiChoiceQATransform(Transform):
+    @staticmethod
+    def transform(
+            x: Dict, y: Dict = None, **kwargs: Any
+    ) -> str:
+        """
+        Transform x and y into a multi-choice format.
+
+        Args:
+            x: a dict with keys "question" and "choices", where "choices" is a list of labels and text.
+            y: a dict with keys "answer" and maybe others.
+            **kwargs: other arguments
+
+        Returns: a string of question, choices, and answer
+
+        """
+        # TODO: Should we use x and y? Maybe only use a single x and check its keys to determine if output is needed.
+        if not isinstance(x, Dict) \
+                or (y and not isinstance(y, Dict)):
+            raise TypeError("x and y should be dict in multi-choice task.")
+
+        transformed = f"Q: {x['question']}\n"
+        transformed += "Answer choices: {}\n".format(
+            " ".join(
+                [
+                    "({}) {}".format(label.lower(), text.lower())
+                    for label, text in zip(
+                    x["choices"]["label"], x["choices"]["text"]
+                )
+                ]
+            )
+        )
+        transformed += "A: "
+
+        if y:
+            if "extraction_words" in kwargs:
+                extraction_words = kwargs["extraction_words"]
+            else:
+                extraction_words = "The answer is"
+            transformed += f"{extraction_words} {y['answer']}"
+
+        return transformed
