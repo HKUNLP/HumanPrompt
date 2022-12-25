a4c2796c9518a9e0c9ddb50523a300c8a4c5a961
BlankCheng <913501223@qq.com>
2022-12-25 17:20:25 +0800

    Add MultiArith dataset for CoT

---
+++ hub/cot/multi_arith/extract_cot_multi_arith.py
@@ -0,0 +1,5 @@
+from humanprompt.components.extract.extract_regex import RegExtract
+
+
+class CoTMultiArithExtract(RegExtract):
+    pass
---
+++ hub/cot/multi_arith/transform_cot_multi_arith.py
@@ -0,0 +1,5 @@
+from humanprompt.components.transform.transform_simple_qa import QATransform
+
+
+class CoTMultiArithTransform(QATransform):
+    pass
