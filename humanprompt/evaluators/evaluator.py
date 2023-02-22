581aafc85f9aa0189a5e19958e3a7d1ec2535ccc
BlankCheng <913501223@qq.com>
2023-02-22 11:02:48 +0800

    Improve evaluator to normalize answers for different datasets

--- humanprompt/evaluators/evaluator.py
+++ humanprompt/evaluators/evaluator.py
@@ -1,4 +1,5 @@
-from typing import Any, Dict, List, Set
+from typing import Any, Dict, List, Set, Union
+import re
 
 import evaluate
 
@@ -11,6 +12,8 @@ class Evaluator:
     def __init__(
         self,
         metrics: List[str],
+        dataset_name: str = None,
+        dataset_subset_name: str = None
     ):
         self.metric_executors = {}
         for metric in metrics:
@@ -18,10 +21,12 @@ class Evaluator:
                 raise ValueError(f"Metric {metric} is not available.")
             # initialize metric executors
             self.metric_executors[metric] = evaluate.load(metric)
+        self.dataset_name = dataset_name
+        self.dataset_subset_name = dataset_subset_name
 
     def evaluate(
         self,
-        predictions: List,  # TODO: Extend input to `Union[List[Dict], Dict[str, Dict]`
+        predictions: List,
         gold_answers: List,
     ) -> Dict[str, Any]:
         """
@@ -33,7 +38,7 @@ class Evaluator:
         Returns:
             A dictionary of evaluation metrics.
         """
-        # TODO: Add support for other metrics, maybe not in `evaluate` package.
+        # TODO: Add support for other metrics not in `evaluate` package.
         eval_dict = dict()
 
         for metric, executor in self.metric_executors.items():
@@ -42,3 +47,68 @@ class Evaluator:
             )
 
         return eval_dict
+
+    def normalize_answer(
+            self,
+            answer: Union[str, List[str]]
+    ) -> Union[str, List[str]]:
+        """
+        Normalize answers according to the dataset.
+        Reference https://github.com/kojima-takeshi188/zero_shot_cot/blob/main/utils.py
+
+        Args:
+            answer: Answer to be normalized.
+        Returns:
+            Normalized answer.
+        """
+        if not isinstance(answer, list):
+            answer = [answer]
+        answer = [str(a).lower() for a in answer]
+
+        for idx, a in enumerate(answer):
+            try:
+                if self.dataset_name in ("gsm8k", "add_sub", "multi_arith", "svamp"):
+                    a = a.replace(",", "")
+                    a = re.findall(r'-?\d+\.?\d*', a)[-1]
+                    # (For arithmetic tasks) if a word ends with period, it will be omitted ...
+                    if a != "":
+                        if a[-1] == ".":
+                            a = a[:-1]
+                    if a[-2:] == ".0":
+                        a = a[:-2]
+                elif self.dataset_name in ("strategy_qa"):
+                    if a == "yes":
+                        a = "true"
+                    elif a == "no":
+                        a = "false"
+                elif self.dataset_name == "glue" and self.dataset_subset_name == "rte":
+                    if a == "true":
+                        a = "0"
+                    elif a == "false":
+                        a = "1"
+                elif self.dataset_name == "glue" and self.dataset_subset_name == "mnli":
+                    if a == "true":
+                        a = "0"
+                    elif a == "neutral":
+                        a = "1"
+                    elif a == "false":
+                        a = "2"
+                elif self.dataset_name == "SetFit/sst5":
+                    if a == "very negative":
+                        a = "0"
+                    elif a == "negative":
+                        a = "1"
+                    elif a == "neutral":
+                        a = "2"
+                    elif a == "positive":
+                        a = "3"
+                    elif a == "very positive":
+                        a = "4"
+            except Exception as e:
+                pass
+            answer[idx] = a
+
+        if len(answer) == 1:
+            return answer[0]
+        else:
+            return answer
